d7585a7e3606562dc81b816e6c0f897927286148
Fiko Aditama <fiko@simise.id>
2024-05-11 14:47:30 +0700

    20240511 - First virus i collect heptoxide

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Computer Virus and Malware Enthusiast
+
+
+
+- [Heptoxide.exe](https://github.com/pankoza2-pl/Heptoxide.exe.git)
---
+++ gitpush.sh
@@ -0,0 +1,6 @@
+git init .
+git add .
+git commit -m "20240511 - First virus i collect heptoxide"
+git branch -M main
+git remote add origin https://github.com/fico-aditama/ComputerVirus.git
+git push -u origin main -f
